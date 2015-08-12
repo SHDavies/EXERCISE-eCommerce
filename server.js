d0f115841e9bf8890e346856de8d6ff6f8249d7d
Spencer Davies <spencerhdavies@gmail.com>
2015-08-12 14:10:35 -0600

    prepping for mongoose

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express'),
 		cors = require('cors'),
-		bodyParser = require('body-parser'),
-		mongojs = require('mongojs');
+		bodyParser = require('body-parser')
 
 var app = express();
 
@@ -10,49 +9,13 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static('./public'));
 
-var db = mongojs('ecommerce', ['products']);
+app.post('/products',})
 
-app.post('/products', function(req, res) {
-	db.products.insert(req.body, function(err, response) {
-		if (!err) {
-			res.json(response);
-		} else {
-			res.status(500).json(err);
-		}
-	})
-})
+app.get('/products',})
 
-app.get('/products', function(req, res) {
-	db.products.find(req.query, function(err, products) {
-		if (!err) {
-			res.json(products);
-		} else {
-			res.status(500).json(err);
-		}
-	})
-})
+app.delete('/products',})
 
-app.delete('/products', function(req, res) {
-	db.products.remove(req.query, function(err, response) {
-		if (err) {
-			res.status(500).json(err);
-		} else {
-			res.json(response);
-		}
-	})
-})
-
-app.put('/products', function(req, res) {
-	console.log(req.query);
-	console.log(req.body);
-	db.products.update(req.query, req.body, function(err, response) {
-		if (!err) {
-			res.json(response);
-		} else {
-			res.status(500).json(err);
-		}
-	})
-})
+app.put('/products',})
 
 
 app.listen(3000, function() {
